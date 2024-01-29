785e6e529ea38c22c1b3783b95b39515171ec117
mark <gh850702@gmail.com>
2024-01-29 20:33:48 +0800

    畫面顯示總金額

--- counter/views.py
+++ counter/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render, redirect
 from .forms import ExpenseModelForm
 from .models import Expense
+from django.db.models import Sum
 # Create your views here.
 
 
 def index(request):
     expenses = Expense.objects.all()  # 查詢所有資料
+    summary = Expense.objects.aggregate(Sum('price'))['price__sum']
     form = ExpenseModelForm()
     if request.method == "POST":
         form = ExpenseModelForm(request.POST)
         if form.is_valid():
             form.save()
         return redirect("/#")
+   # print(summary)
     context = {
         'form': form,
         'expenses': expenses,
+        'sum': summary
     }
     return render(request, 'expenses/index.html', context)
 
